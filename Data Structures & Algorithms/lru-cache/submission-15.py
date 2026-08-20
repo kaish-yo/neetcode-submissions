c8820bbee2d13e61d73e57a7fc1be72883030e5c
kaish-yo <58113658+kaish-yo@users.noreply.github.com>
2026-08-20 18:24:37 +0900

    Add: lru-cache - submission-15

---
+++ Data Structures & Algorithms/lru-cache/submission-15.py
@@ -0,0 +1,25 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+
+    def get(self, key: int) -> int:
+        if not key in self.cache:
+            return -1
+        
+        val = self.cache.pop(key)
+        self.cache[key] = val
+        return val        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.pop(key)
+        elif len(self.cache) >= self.capacity:
+            self.cache.pop(next(iter(self.cache)))
+        
+        self.cache[key] = value
+        return
+
+        
+
