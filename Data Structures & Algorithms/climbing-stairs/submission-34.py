59672115b67c2e4013f915df1881343a9e367fbe
kaish-yo <58113658+kaish-yo@users.noreply.github.com>
2026-09-04 13:14:00 +0900

    Add: climbing-stairs - submission-34

---
+++ Data Structures & Algorithms/climbing-stairs/submission-34.py
@@ -0,0 +1,21 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        memo = {}
+        
+        def dfs(i):
+            # Base cases
+            if i > n:
+                return 0
+            if i == n:
+                return 1
+            
+            # Recurring cases
+            if not i - 1 in memo:
+                memo[i + 1] = dfs(i + 1)
+            if not i - 2 in memo:
+                memo[i + 2] = dfs(i + 2)
+            
+            return memo[i + 1] + memo[i + 2]
+        
+        return dfs(0)
